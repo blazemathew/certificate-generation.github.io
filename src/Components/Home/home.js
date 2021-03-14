5597730188f9fea2ebaea0261de2a141e409131e
blazemathew <blazemathew987@gmail.com>
2021-03-14 18:38:20 +0530

    style fixed

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './home.scss';
 
 const Home = () => {
     return(
-        <div>
+        <div className="home">
             <h1>Certificate Generate Page</h1>
             <h2>Nullcast is the best play ground to learn new things.</h2>
         </div>
